764c93ba86624cf9f2bcfc2325c226cd612e0b56
jp3864 Jessica Peng <jp3864@columbia.edu>
2021-10-19 04:31:12 +0000

    got rid of comments

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -2,15 +2,13 @@
 
 Given /the following movies exist/ do |movies_table|  
   movies_table.hashes.each do |movie|
- # print("\n")
-#  print(movie)
-  Movie.create!(movie)
+  # push moves into database
+  # each returned element will be a hash whose key is the table header.
+  # you should arrange to add that movie to the database here.
   
-      # push movies to database
-      
-    # push moves into database
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+  Movie.create!(movie)
+        
+  
   end
   # fail "Unimplemented"
 end
@@ -19,22 +17,16 @@ Then /(.*) seed movies should exist/ do | n_seeds |
   Movie.count.should be n_seeds.to_i
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
-
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2| 
    #check index of e1 appears before e2 
-   #  page.body is the entire content of the page as a string.
+   #page.body is the entire content of the page as a string.
 
-   # print(page.body.class)
     html = page.body
     if html.index(e1) < html.index(e2)
         ret = true
     else
         ret = false
     end 
-  
-# fail "Unimplemented"
   ret 
 end
 
@@ -49,16 +41,6 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
     else
         ratings.each{|x| check(x)}
     end 
-  #  print("hellooooo")
- #   print("uncheck is")
- #   print(uncheck)
- #   print("ratings_list is")
- #   print(rating_list)
-  #  print("\n")
-  #  print("done")
-  #  
-  #  ratings.each{|x| I uncheck x}
-  
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
@@ -75,7 +57,6 @@ Then /I should see all the movies/ do
       end
   }
   ret = true
-  # fail "Unimplemented"
   ret
 end
 
